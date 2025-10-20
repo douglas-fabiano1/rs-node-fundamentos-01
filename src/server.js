a2ca06ce0d9106f335a78cf382eab9ffe80fa366
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2025-10-20 15:25:53 -0300

    Estrutura da aplicação - Salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -1,14 +1,24 @@
 import http from 'node:http'
 
+// Array users
+const users = []
+
 const server = http.createServer((request, response) => {
     const {method, url } = request;
 
     if (method === 'GET' && url === '/users') {
         // Early return
-        return response.end('Listagem de usuários')
+        return response
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
     }
 
     if(method === 'POST' && url === '/users'){
+        users.push({
+            id: 1,
+            name: 'Douglas',
+            email: 'douglas@example.com'
+        })
         return response.end('Criação de usuário')
     }
 
