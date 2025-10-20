393043ce796a09a2827a90cca77986c98ec11bda
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2025-10-20 18:03:52 -0300

    Entendendo Streams no Node.js

---
+++ streams/fundamentals.js
@@ -0,0 +1,13 @@
+// Netflix & Spotify
+
+// Importação de clientes via CSV (Excel)
+// 1GB - 1.000MB(base decimal) ou 1024MB(base binária)
+// POST /upload import.csv
+
+// 10MB/s - 100s
+
+// 100s -> inserção no BD
+
+// 10MB/s -> +- 10.000
+
+// Readable Streams / Writable Streams
