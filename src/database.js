4f82ad4828c87c1774596f7487120b4a8c93a9a3
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2025-10-22 16:43:31 -0300

    Criando ID único e universal (UUID)

--- src/database.js
+++ src/database.js
@@ -1,10 +1,7 @@
-// { "users": [...] } 
 import fs from 'node:fs/promises'
 import { URL } from 'node:url'
 
-// Deixa a criação do arquivo db.json relativa ao arquivo database.js
 const databasePath = new URL('../db.json', import.meta.url)
-console.log(databasePath)
 
 export class Database {
     #database = {}
@@ -12,28 +9,22 @@ export class Database {
     constructor(){
         fs.readFile(databasePath, 'utf8')
             .then(data => {
-                this.#database = JSON.parse(data) // parse para parsear os dados
+                this.#database = JSON.parse(data)
             }).catch(() => {
-                this.#persist() // Cria o arquivo, mesmo se estiver vazio.
+                this.#persist() 
             })
     }
 
-    // Método que vai escrever o nosso BD em um arquivo físico.
-    // stringify para escrever os dados
     #persist() {
         fs.writeFile(databasePath, JSON.stringify(this.#database))
     }
 
-    // Método de seleção da tabela
     select(table) {
-        // verifica se há uma chave no BD com o nome de tabela,
-        // caso não exista, retorna array vazio.
         const data = this.#database[table] ?? []
 
         return data
     }
 
-    // Método de inserção de dados na tabela
     insert(table, data) {
         if (Array.isArray(this.#database[table])) {
             this.#database[table].push(data)
--- streams/buffer.js
+++ streams/buffer.js
@@ -1,4 +1,3 @@
 const buf = Buffer.from("Douglas")
-// <Buffer 44 6f 75 67 6c 61 73>
-// Node consegue trabalhar de maneira mais performática com hexadecimais
+
 console.log(buf.toJSON())
