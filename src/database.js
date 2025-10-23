dcdc1fd2c325004cb9c0d077a6ff9169568ffa1b
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2025-10-23 19:34:37 -0300

    Remoção de registros

--- src/database.js
+++ src/database.js
@@ -5,13 +5,13 @@ const databasePath = new URL('../db.json', import.meta.url)
 
 export class Database {
     #database = {}
-    
-    constructor(){
+
+    constructor() {
         fs.readFile(databasePath, 'utf8')
             .then(data => {
                 this.#database = JSON.parse(data)
             }).catch(() => {
-                this.#persist() 
+                this.#persist()
             })
     }
 
@@ -35,5 +35,14 @@ export class Database {
         this.#persist()
 
         return data
+    }
+
+    delete(table, id) {
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+        if (rowIndex > -1) {
+            this.#database[table].splice(rowIndex, 1)
+            this.#persist() // salva o BD local
+        }
     }
 }
