5d875a700ff14abdd354c76c7dc59e06e5a74b44
Douglas Fabiano <contatodouglasfabiano1@gmail.com>
2025-10-23 09:48:31 -0300

    Route e Query parameters

--- src/server.js
+++ src/server.js
@@ -2,17 +2,31 @@ import http from 'node:http'
 import { json } from './middlewares/json.js';
 import { routes } from './routes.js'
 
+// Formas de uma aplicação(front-end) consumindo nossa API enviar infos para nossa API
+// 1 - Query Parameters: userId -> chave; 1 -> valor (uso para URL Stateful e infos não sensíveis => filtros, paginação, não-obrigatórios)
+// 2 - Route Parameters: 1 -> usado para identificação de recurso
+// 3 - Request Body: Envio de informações de um formulário geralmente (passam pelo protocolo HTTPs, ou seja, muito mais difíceis de serem descriptografados)
+
+// Exemplos:
+
+// 1 - http://localhost:3333/users?userId=1&name=Douglas ; caso queira informar mais parâmetros, uso o '&'
+// 2 - GET http://localhost:3333/users/1 ; estou buscando usuário com id 1 ; o método http basicamente já diz o que o 1 significa
+// 2 - é uma combinação de método(GET) + recurso(users) + Route Parameter para entender o que a rota quer dizer
+// 1 e 2 - ficam na URL - ambos não devem ser usados para envio de informações sensíveis. ex: passwords
+// 3 - POST http://localhost:3333/users
+
+// Edição e Remoção de usuário:
+
+
 const server = http.createServer(async (request, response) => {
     const { method, url } = request;
 
     await json(request, response)
 
-    // verifica se a rota existe
     const route = routes.find(route => {
         return route.method === method && route.path === url
     })
 
-    // caso tenha encontrado a rota
     if(route){
         return route.handler(request, response)
     }
